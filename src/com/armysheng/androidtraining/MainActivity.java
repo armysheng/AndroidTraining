f0dc26251fda0886ca322e66b3003c84ad11544c
armysheng <armysheng@gmail.com>
2015-01-23 17:20:03 +0800

    Building your first APP

--- src/com/armysheng/androidtraining/MainActivity.java
+++ src/com/armysheng/androidtraining/MainActivity.java
@@ -3,30 +3,37 @@ package com.armysheng.androidtraining;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.os.Build;
 
 
 
 public class MainActivity extends Activity {
 
-    @Override
+    public static final String EXTRA_MESSAGE = "com.armysheng.androidtraining";
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
-
+    /** 当用户点击按钮时会被调用 */
+    public void sendMessage(View view) {
+        // 响应按钮的事件处理逻辑
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
